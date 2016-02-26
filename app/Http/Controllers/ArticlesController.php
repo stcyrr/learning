169fa16291c99a903dd723bde54f74ae9c940d50
stcyrr <stcyrr@videotron.ca>
2016-02-26 17:27:48 -0500

    Addition of create

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,17 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;use App\Article;
-
+use App\Article;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use Request;
 
 class ArticlesController extends Controller
 {
     //
     public function index()
     {
-        $articles = Article::all();
+        $articles = Article::latest('published_at')->get();
         return view('articles.index')->with('articles', $articles);
     }
 
@@ -21,5 +22,18 @@ class ArticlesController extends Controller
         $article = Article::findOrFail($id);
 
         return view('articles.show')->with('article', $article);
+    }
+
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    public function store()
+    {
+        $input = Request::all();
+        $input['published_at'] = Carbon::now();
+        Article::create($input);
+        return redirect('articles');
     }
 }
