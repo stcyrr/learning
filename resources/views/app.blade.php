617a23e77a51cdfb5058bb46d540cfce62b06cc7
stcyrr <stcyrr@videotron.ca>
2016-02-26 11:58:03 -0500

    Added contact page

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Articles</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+    	@yield('content')
+    </div>
+
+    @yield('footer')
+
+</body>
+</html>
